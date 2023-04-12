73669fe0e336f5dfd3f49c5878a0ef715de017fa
mak-aravind <aravind.murugaiyan@gmail.com>
2023-04-12 12:43:51 +0000

    validate overlapping dates for membership

--- library_management/library_management/doctype/librarymember/librarymember.py
+++ library_management/library_management/doctype/librarymember/librarymember.py
@@ -8,3 +8,6 @@ from frappe.model.document import Document
 class LibraryMember(Document):
 	def before_save(self):
 		self.full_name = f'{self.first_name} {self.last_name}';
+	# to test the controller code from outside the below method introduced
+	def getFullName(self):
+		return self.full_name;
